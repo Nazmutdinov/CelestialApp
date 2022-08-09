79fd87105b11da0009e46ba7d1f4302db7d42261
admin <compareto@mail.ru>
2022-08-09 12:43:17 +0500

    remove russian comments at AddTagCelestialUseCase.kt

--- app/src/main/java/com/example/celestialapp/domain/usecase/AddTagCelestialUseCase.kt
+++ app/src/main/java/com/example/celestialapp/domain/usecase/AddTagCelestialUseCase.kt
@@ -8,66 +8,51 @@ import com.example.celestialapp.domain.repository.LocalDataRepository
 import java.util.*
 
 /**
- * сохранить привязку небесного тела к тегу, сохранить тег в БД
+ * save tag into db, save binding tag to celestial into db
  */
 class AddTagCelestialUseCase(
     private val context: Context,
     private val localDataRepository: LocalDataRepository
 ) {
     suspend operator fun invoke(
-        // имя тега
         tagName: String,
-        // id небесного тела
         favouriteCelestialDataItem: FavouriteCelestialDataItem
     ): ResourceUseCase<Boolean> {
-        // проверим есть ли в БД такой же тег?
-        val existsTags = isTagExisting(tagName)
-
+        val existsTags = isTagExistingInDb(tagName)
         if (existsTags) return ResourceUseCase.Error(context.getString(R.string.insertKeywordDataUseCaseError))
 
-        // такого тега еще нет в БД, добавим тег
+        // if there is not this tag at db, then add tag
         val resourceInsertTag = localDataRepository.insertTagData(tagName)
-
         if (resourceInsertTag is Resource.Error) return ResourceUseCase.Error("${resourceInsertTag.message}")
-        // тег успешно добавлен, достанем keyword id
+
         resourceInsertTag.data?.let { keywordId ->
-            // сохраним привязку небесного тела и тега в БД
-            val resource =
+            val resourceCrossRef =
                 localDataRepository.insertCelestialTagsCrossRef(
                     favouriteCelestialDataItem.celestialId,
                     keywordId
                 )
 
-            // обновим дату сохранения тела в избранном
-            updateFavouriteDate(favouriteCelestialDataItem.nasaId)
+            updateDateForCelestialIntoDb(favouriteCelestialDataItem.nasaId)
 
-            return if (resource is Resource.Success) ResourceUseCase.Success(true)
-                else ResourceUseCase.Error("${resource.message}"
-            )
+            return if (resourceCrossRef is Resource.Success) ResourceUseCase.Success(true)
+            else ResourceUseCase.Error("${resourceCrossRef.message}")
         }
 
         return ResourceUseCase.Error(context.getString(R.string.nonDefineError))
     }
 
-    /**
-     *проверяем, есть уже такой тег в БД
-     */
-    private suspend fun isTagExisting(tagName: String): Boolean {
-        val tagId = localDataRepository.getTagIdByName(tagName)
+    private suspend fun isTagExistingInDb(tagName: String): Boolean {
+        val tagId = localDataRepository.getTagIdByName(tagName).data
 
-        return tagId.data != null
+        return tagId != null
     }
 
-    /**
-     * обновление даты сохранения тела в избранном
-     */
-    private suspend fun updateFavouriteDate(nasaId: String) {
-        val resource =  localDataRepository.getFavouriteDate(nasaId)
 
-        // дата пустая, то сохрани м текущую
-        if (resource is Resource.Error) {
-            localDataRepository.updateFavouriteDate(nasaId, Date().time)
-        }
+    private suspend fun updateDateForCelestialIntoDb(nasaId: String) {
+        val resource = localDataRepository.getFavouriteDate(nasaId)
+
+        // if there in no date in db, then save current date into db
+        if (resource is Resource.Error) localDataRepository.updateFavouriteDate(nasaId, Date().time)
     }
 }
 
